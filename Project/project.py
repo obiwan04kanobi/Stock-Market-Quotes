cc29c2ab859a148bfc26e63f16de2a4ec4f23403
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-08-31 16:43:27 +0530

    added "display_result" and "stock_search"

--- Project/project.py
+++ Project/project.py
@@ -1,27 +1,26 @@
 import requests
 import sys
-import json
+from tabulate import tabulate
+
 
 def main():
-    print("Do you have an API KEY (Y for Yes & N for NO) ?")
-    api_key_bool = input("Enter: ").upper()
 
-    api_key(api_key_bool)
+    api_val = api_key()
 
-    if api_key(api_key_bool) == None:
-        sys.exit("Enter Y for Yes & N for NO ")
-
-    print("Enter Country: ")
-    country_name = input("Country Name: ").strip().title()
+    print("Enter Country Name")
+    country_name = input("Country: ").strip().title()
 
     valid_country_name(country_name)
 
-    status = market_status(country_name, api_key_bool)
-    print(f"Market is {status}")
+    status = market_status(country_name)
+    print(f"Market is {status}\n")
 
-    print("Enter Stock Ticker Symbol: ")
-    company_name = input("Ticker Symbol: ").strip().upper()
+    print("Search Stock Ticker Symbol: ")
+    company_name = input("Search 🔎: ").strip().title()
 
+    stock_search(company_name, country_name, api_val)
+
+    display_result(api_val)
 
 
 # FUNCTION 1
@@ -51,55 +50,96 @@ def valid_country_name(country_name):
 
 
 # FUNCTION 2
-def market_status(country_name, api_key_bool):
-    if api_key(api_key_bool) == False:
+def market_status(country_name):
+
+    response = requests.get(
+        "https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo"
+    )
+    data = response.json()
+
+    for market in data["markets"]:
+        if market["region"] == country_name:
+            if market["current_status"] == "open":
+                return market["current_status"]
+
+            else:
+                return market["current_status"]
+
+        else:
+            continue
+
+
+# FUNCTION 3
+def stock_search(ticker, country_name, api_val):
+    try:
+
         response = requests.get(
-            "https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo"
+            f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}&apikey={api_val}"
         )
         data = response.json()
 
-        for market in data["markets"]:
-            if market["region"] == country_name:
-                if market["current_status"] == "open":
-                    return market["current_status"]
+        # Extract the list of dictionaries under "bestMatches"
+        best_matches_data = data["bestMatches"]
 
-                else:
-                    return market["current_status"]
+        # Filter entries based on the desired region
+        filtered_entries = [
+            entry
+            for entry in best_matches_data
+            if entry["4. region"] == country_name
+        ]
 
-            else:
-                continue
+        # Convert each filtered dictionary into a list of key-value pairs
+        table_data = [
+            [f"{entry['1. symbol']}: {entry['2. name']}", entry["8. currency"]]
+            for entry in filtered_entries
+        ]
 
-    elif api_key(api_key_bool) == True:
-        api_key_value = input("API Key: ")
+        # Print the data in tabular format
+        table_headers = ["Symbol and Name", "Currency"]
+        table = tabulate(table_data, headers=table_headers, tablefmt="grid")
 
-        response = requests.get(
-            f"https://www.alphavantage.co/query?function=MARKET_STATUS&apikey={api_key_value}"
-        )
-        data = response.json()
+        # Print the formatted table
+        print(table)
 
-        for market in data["markets"]:
-            if market["region"] == country_name:
-                if market["current_status"] == "open":
-                    return market["current_status"]
-
-                else:
-                    return market["current_status"]
-
-            else:
-                continue
+    except KeyError:
+        sys.exit("Try Again with API KEY")
 
 
 # FUNCTION 4
-def api_key(api_key_bool):
-    if api_key_bool == "Y":
-        print("API Request Limit: 5 API requests per minute and 100 requests per day\n")
-        return True
+def api_key():
 
-    elif api_key_bool == "N":
-        return False
+    print(
+        "Requests Limit ==> up to 5 API requests per minute and 100 requests per day"
+    )
+    api_key_value = input("API Key: ")
+    return api_key_value
 
-    else:
-        return None
+
+# FUNCTION 5
+def display_result(api_val):
+    stock_name = input("TICKER: ")
+    try:
+
+        response = requests.get(
+            f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock_name}&apikey={api_val}"
+        )
+        data = response.json()
+
+        # Extract the inner dictionary under "Global Quote"
+        quote_data = data["Global Quote"]
+
+        # Convert the inner dictionary into a list of key-value pairs
+        table_data = [[key, value] for key, value in quote_data.items()]
+
+        # Print the data in tabular format
+        table_headers = ["Field", "Value"]
+        table = tabulate(table_data, headers=table_headers, tablefmt="rounded_grid")
+
+        # Print the formatted table
+        print(table)
+
+    except KeyError:
+        sys.exit("Try again with API KEY")
 
 
 if __name__ == "__main__":
